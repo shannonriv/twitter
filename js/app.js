8912aee22ffb0facd4cb0bb6f26457643debcd2c
shannonriv <shannonriv18@gmail.com>
2018-01-22 11:59:27 -0500

    cargando estructura mejorada y funcion twittear

--- js/app.js
+++ js/app.js
@@ -1,16 +1,41 @@
-var area = document.getElementById('write-msg');
-var button = document.getElementById('tweet');
-var container = document.getElementById('container-msgs');
+window.addEventListener('load', function() {
+  var content = document.getElementById('write-tweet');
+  var posting = document.getElementById('tweet');
+  
+  content.addEventListener('keyup', btnactive);
+  posting.addEventListener('click', send);
 
-button.addEventListener('mouseover', function(event) {
-    if(area.value) {
-        var message = document.createElement('p');
-
-        message.textContent = area.value;
-        message.setAttribute('class', 'tweeted')
-
-        container.appendChild(message);
-
-        area.value = '';
+  function btnactive(event) {
+    if (content.value === '') {
+      posting.disabled = true;
+      posting.style.backgroundColor = 'gainsboro';
+      posting.style.cursor = 'not-allowed';      
+    } else {
+      posting.disabled = false;
+      posting.style.backgroundColor = 'lightblue';
+      posting.style.cursor = 'pointer';
     }
+  };
+
+  function send(event) {
+    var containerTweets = document.getElementById('container-tweets');
+    var newPost = document.createElement('div');
+
+    newPost.classList.add('newPost');
+    newPost.textContent = content.value;
+    
+    var hours = document.createElement('div');
+    hours.classList.add('hours');
+    var date = new Date();
+    var hour = date.getHours();
+    var hourPost = 'Posted at ' + hour + 'h';
+    hours.textContent = hourPost;
+
+    newPost.appendChild(hours);
+    containerTweets.prepend(newPost);
+
+    content.value = '';
+
+    btnactive(); 
+  };
 });
